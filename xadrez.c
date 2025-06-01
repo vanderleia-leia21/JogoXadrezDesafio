a0c2507a4852306dbd2d1ba155ffa0b0e03b78e3
Vanderleia <vanderleiasantosdaconceicaosan@gmail.com>
2025-06-01 11:16:48 +0000

    Criando o primeiro movimento do Bispo

--- xadrez.c
+++ xadrez.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+ int main(){
+int x = 0, y = 0; // Posição inicial comum
+
+    // ===== Movimento do Bispo (diagonal superior direita x++, y++) usando for =====
+    printf("Movimento do Bispo: Cima/Direita\n");
+    for (int i = 0; i < 5; i++) {
+        x++;
+        y++;
+        printf("Cima/Direita: %d casa\n", x,y);
+    }
+
+ return 0;
+ }
